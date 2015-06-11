f93f6ffed7777abfa086891f9b8ba88d15a36fff
Monster <teosmail@web.de>
2015-06-11 20:40:22 +0200

    Added visible and events to mouse

--- src/main/python/mbge/mouse.py
+++ src/main/python/mbge/mouse.py
@@ -20,5 +20,18 @@ def y(self):
 def y(self, y):
     bge.render.setMousePosition(self.x, y)
 
+@property
+def visible(self):
+    'The visibility of the system mouse cursor'
+    return bge.render.mouse.visible
+
+@visible.setter
+def visible(self, visible):
+    bge.render.mouse.visible = visible
+
+@property
+def events(self):
+    'All current mouse events (readonly)'
+    return bge.render.mouse.events
 
 import mprop; mprop.init()
